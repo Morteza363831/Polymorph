268121fef675f10624000567042997b297b9f93f
ruzstock.ir <ali16mar15couples@gmail.com>
2024-07-17 10:35:58 +0430

    project created

---
+++ src/main/java/com/example/polymorph/PolymorphApplication.java
@@ -0,0 +1,13 @@
+package com.example.polymorph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolymorphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolymorphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/polymorph/PolymorphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polymorph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolymorphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
